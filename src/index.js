4a23e1e956f921d315371661789df4944835fb9e
katsura919 <katsuragik919@gmail.com>
2025-09-05 14:15:10 +0800

    feat: Add amenity routes to the main router for improved resource management

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const authRoutes = require('./modules/auth/auth-routes');
 const userRoutes = require('./modules/user/user-routes');
 const resortRoutes = require('./modules/resort/resort-route');
 const roomRoutes = require('./modules/room/room-routes');
+const amenityRoutes = require('./modules/amenity/amenity-routes');
 
 const reservationRoutes = require('./modules/reservation/reservation-routes');
 const chatRoutes = require('./modules/chat/chat-routes');
@@ -14,6 +15,7 @@ router.use('/auth', authRoutes);
 router.use('/user', userRoutes);
 router.use('/resort', resortRoutes);
 router.use('/room', roomRoutes);
+router.use('/amenity', amenityRoutes);
 router.use('/reservation', reservationRoutes);
 router.use('/feedback', feedbackRoutes);
 router.use('/chat', chatRoutes);
